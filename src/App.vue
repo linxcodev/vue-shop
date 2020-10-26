642bd74c17ce2c47fc99eb1a3af60f9e1664ddd6
Fajrul <fajrul452@gmail.com>
2020-10-26 09:48:38 +0700

    Layout header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,32 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      Header
+    <v-app-bar app color="primary" dark>
+     <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+     <v-toolbar-title>Vueshop</v-toolbar-title>
+
+     <!-- pemisah konten -->
+     <v-spacer></v-spacer>
+
+     <v-btn icon>
+      <v-badge color="orange" overlap>
+        <template v-slot:badge>
+          <span>3</span>
+        </template>
+
+        <v-icon>mdi-cart</v-icon>
+      </v-badge>
+     </v-btn>
+
+     <v-text-field
+       slot="extension"
+       hide-details
+       append-icon="mdi-microphone"
+       flat
+       label="Search"
+       prepend-inner-icon="mdi-magnify"
+       solo-inverted
+      ></v-text-field>
     </v-app-bar>
 
     <v-navigation-drawer app>
