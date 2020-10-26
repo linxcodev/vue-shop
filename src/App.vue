aa3ab5faf357cb7cd2824fd635b52afdeec34394
Fajrul <fajrul452@gmail.com>
2020-10-26 10:12:26 +0700

    Layout main, sidebar, footer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-     <v-app-bar-nav-icon></v-app-bar-nav-icon>
+     <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
 
      <v-toolbar-title>Vueshop</v-toolbar-title>
 
@@ -29,25 +29,89 @@
       ></v-text-field>
     </v-app-bar>
 
-    <v-navigation-drawer app>
-      Side Menu
-    </v-navigation-drawer>
+    <!-- sidebar -->
+    <v-card>
+     <v-navigation-drawer app v-model="drawer">
+       <!-- Profile User -->
+       <v-list-item v-if="!guest">
+         <v-list-item-avatar>
+           <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+         </v-list-item-avatar>
+
+         <v-list-item-content>
+           <v-list-item-title>John Leider</v-list-item-title>
+         </v-list-item-content>
+       </v-list-item>
+
+       <div class="pa-2" v-if="guest">
+         <v-btn block color="primary" class="mb-1">
+           <v-icon left>mdi-lock</v-icon>
+           Login
+         </v-btn>
+
+         <v-btn block color="success">
+           <v-icon left>mdi-account</v-icon>
+           Register
+         </v-btn>
+       </div>
+
+       <v-divider></v-divider>
+
+       <v-list>
+         <v-list-item
+         v-for="(item, index) in menus"
+         :key="`menu-`+index"
+         :to="item.route"
+         >
+           <v-list-item-icon>
+             <v-icon left>{{ item.icon }}</v-icon>
+           </v-list-item-icon>
+
+           <v-list-item-content>
+             <v-list-item-title>{{ item.title }}</v-list-item-title>
+           </v-list-item-content>
+         </v-list-item>
+       </v-list>
+
+       <template v-slot:append v-if="!guest">
+         <div class="pa-2">
+           <v-btn block color="red" dark>
+             <v-icon left>mdi-lock</v-icon>
+             Logout
+           </v-btn>
+         </div>
+       </template>
+     </v-navigation-drawer>
+    </v-card>
 
     <v-content>
       <v-container fluid>
-        <!-- jika menggunakan vue-router -->
-        <router-view></router-view>
+        <v-slide-y-transition mode="out-in">
+          <router-view></router-view>
+        </v-slide-y-transition>
       </v-container>
     </v-content>
 
-    <v-footer app>
-      Footer
-    </v-footer>
+    <v-card>
+     <v-footer absolute app>
+       <v-card-text class="text-center">
+         &copy; {{ new Date().getFullYear() }} — <strong>Vueshop</strong>
+       </v-card-text>
+     </v-footer>
+    </v-card>
   </v-app>
 </template>
 
 <script>
 export default {
- name: 'App'
-}
+   name: 'App',
+   data: () => ({
+     drawer: false,
+     menus: [
+       { title: 'Home', icon: 'mdi-home', route: '/' },
+       { title: 'About', icon: 'mdi-account', route: '/about' },
+     ],
+     guest: true,
+   })
+};
 </script>
